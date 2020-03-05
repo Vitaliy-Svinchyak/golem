2bd7818cc51f405681a58d684ecaafdcf603afdd
e33 <vitalii.svinchyak@gmail.com>
2020-03-05 17:07:08 +0200

    New zombie comparator

--- src/main/java/com/example/e33/util/ZombieComparator.java
+++ src/main/java/com/example/e33/util/ZombieComparator.java
@@ -2,8 +2,7 @@ package com.example.e33.util;
 
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.monster.ZombieEntity;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraft.entity.passive.GolemEntity;
 
 import java.util.Comparator;
 
@@ -15,30 +14,67 @@ public class ZombieComparator implements Comparator<ZombieEntity> {
     }
 
     public int compare(ZombieEntity mob1, ZombieEntity mob2) {
-        if (!mob1.isBurning() && mob2.isBurning()) {
+        int mob1HazardPoints = this.getHazardPoints(mob1);
+        int mob2HazardPoints = this.getHazardPoints(mob2);
+
+        if (mob1HazardPoints > mob2HazardPoints) {
+            return -1;
+        } else if (mob1HazardPoints < mob2HazardPoints) {
             return 1;
         }
-        if (mob1.isBurning() && !mob2.isBurning()) {
-            return -1;
+
+        return 0;
+    }
+
+    private int getHazardPoints(ZombieEntity mob) {
+        int hazardPoints = 0;
+
+        if (mob.isBurning()) {
+            return -100;
         }
 
-        if (mob1.hurtResistantTime > 10.0F && mob2.hurtResistantTime <= 10.0F) {
-            return 1;
-        }
-        if (mob1.hurtResistantTime <= 10.0F && mob2.hurtResistantTime > 10.0F) {
-            return -1;
+        if (mob.getAttackTarget() instanceof GolemEntity) {
+            hazardPoints += 10;
         }
 
-        if (!mob1.isChild() && mob2.isChild()) {
-            return 1;
-        }
-        if (mob1.isChild() && !mob2.isChild()) {
-            return -1;
+        if (mob.isChild()) {
+            hazardPoints += 5;
         }
 
-        double s1Distance = this.creature.getDistanceSq(mob1);
-        double s2Distance = this.creature.getDistanceSq(mob2);
+        double distanceToMob = this.creature.getDistanceSq(mob);
 
-        return (int) Math.floor(s1Distance - s2Distance);
+        if (distanceToMob <= 10) {
+            hazardPoints += 2;
+        }
+        if (distanceToMob <= 5) {
+            hazardPoints += 4;
+        }
+        if (distanceToMob > 10) {
+            hazardPoints -= 3;
+        }
+
+        if (mob.hurtResistantTime <= 10.0F) {
+            hazardPoints += 1;
+        }
+        if (mob.hurtResistantTime > 10.0F) {
+            hazardPoints -= 1;
+        }
+
+        final int[] equipmentPower = {0};
+        mob.getArmorInventoryList().forEach(itemStack -> {
+            equipmentPower[0]++;
+            if (itemStack.isEnchanted()) {
+                equipmentPower[0]++;
+            }
+        });
+        mob.getHeldEquipment().forEach(itemStack -> {
+            equipmentPower[0]++;
+            if (itemStack.isEnchanted()) {
+                equipmentPower[0]++;
+            }
+        });
+        hazardPoints += equipmentPower[0];
+
+        return hazardPoints;
     }
 }
