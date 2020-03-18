c9fc3c9f78195a57d02bd3e47536b9cd41b4b66f
e33 <vitalii.svinchyak@gmail.com>
2020-03-18 13:10:08 +0200

    Render refactoring (#4)
    Refactoring

---
+++ src/main/java/com/e33/client/animation/animationProgression/CubeAnimationProgression.java
@@ -0,0 +1,43 @@
+package com.e33.client.animation.animationProgression;
+
+import com.e33.client.util.ModelBoxParameters;
+import net.minecraft.client.renderer.entity.model.RendererModel;
+import net.minecraft.client.renderer.model.ModelBox;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class CubeAnimationProgression extends AnimationProgression {
+
+    private int cubeNumber;
+    private ModelBoxParameters originalModelBoxParameters;
+
+    CubeAnimationProgression(RendererModel rendererModel, List<Float> xProgression, List<Float> yProgression, List<Float> zProgression, ProgressionType progressionType, int cubeNumber, ModelBoxParameters originalModelBoxParameters) {
+        super(rendererModel, xProgression, yProgression, zProgression, progressionType);
+
+        this.cubeNumber = cubeNumber;
+        this.originalModelBoxParameters = originalModelBoxParameters;
+    }
+
+    protected void progress(float newX, float newY, float newZ) {
+        switch (this.progressionType) {
+            case ModelBoxPosition:
+                ModelBoxParameters p = this.originalModelBoxParameters;
+                ModelBox newCube = new ModelBox(p.model, p.textureOffsetX, p.textureOffsetY, newX, newY, newZ, p.width, p.height, p.depth, p.scaleFactor, p.mirrored);
+                this.rendererModel.cubeList.set(this.cubeNumber, newCube);
+                this.setCubesNotCompiled();
+                break;
+        }
+    }
+
+    private void setCubesNotCompiled() {
+        try {
+            Field f = this.rendererModel.getClass().getDeclaredField("compiled"); //NoSuchFieldException
+            f.setAccessible(true);
+            f.set(this.rendererModel, false); //IllegalAccessException
+
+        } catch (ReflectiveOperationException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+}
